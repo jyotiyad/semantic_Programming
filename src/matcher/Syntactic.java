bdb9eb0edc58bad2a3ba5efc75e3becadb5c8867
Larry Poon <lpoon2@illinois.du>
2017-02-28 01:49:18 +0100

    calculate similiarity

--- src/matcher/Syntactic.java
+++ src/matcher/Syntactic.java
@@ -13,6 +13,7 @@ import javax.xml.xpath.XPathFactory;
 import java.awt.*;
 import java.io.File;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import static com.hp.hpl.jena.vocabulary.OWLResults.system;
@@ -26,8 +27,11 @@ public class Syntactic {
     public static String   out = "output";
     public static String   msg = "message";
     public static String   part = "part";
-    public Map<String, Map<String, String>> lookup2 ;
+    private static EditDistance EditDistance;
 
+    /*
+    separate a string by the position of the colon
+     */
     public static String splitString(String s){
         if(s.contains(":")){
             String[] ss = s.split(":");
@@ -46,7 +50,12 @@ public class Syntactic {
         return add;
     }
 
-    public static void parseDOM(String fileName){
+    /*
+        parseDOM takes in a file name in String and returns a hashmap with operation names as keys, another hashmap as values.
+        The inner hashmap stores the output and input messages.
+     */
+    public static Map<String, Map<String, String>> parseDOM(String fileName){
+         Map<String, Map<String, String>> ret = new HashMap<String , Map<String, String>>();
 
             try {
                 File input = new File(fileName);
@@ -70,6 +79,7 @@ public class Syntactic {
 
 
                     try {
+
                         String in_s = splitString(ele.getElementsByTagName(in).item(0).getAttributes().getNamedItem("message").getNodeValue());
                         String out_s = splitString(ele.getElementsByTagName(out).item(0).getAttributes().getNamedItem("message").getNodeValue());
 
@@ -82,36 +92,62 @@ public class Syntactic {
                         Node outNode = (Node) xPath.compile(xPath_out).evaluate(doc, XPathConstants.NODE);
                         Element pEle_out = (Element) outNode;
                         try {
+                          //  if(pEle.getElementsByTagName(part).item(0).getAttributes().getNamedItem("type") == null)
                             io.put("input", splitString(pEle.getElementsByTagName(part).item(0).getAttributes().getNamedItem("element").getNodeValue()));
+
+                           // io.put("input", splitString(pEle.getElementsByTagName(part).item(0).getAttributes().getNamedItem("type").getNodeValue()));
+
                             io.put("output", splitString(pEle_out.getElementsByTagName(part).item(0).getAttributes().getNamedItem("element").getNodeValue()));
+
+                            //io.put("output", splitString(pEle_out.getElementsByTagName(part).item(0).getAttributes().getNamedItem("type").getNodeValue()));
+
                         }
                         catch (Exception e){
+                                //     e.printStackTrace();
+
                             io.put("input", "result");
                             io.put("output", "response");
 
                         }
 
-                        lookup1.put(name, io);
+                        ret.put(name, io);
                     }
                     catch (Exception e){
                //         e.printStackTrace();
                     }
 
                 }
-                    System.out.println(lookup1.get("InstallPaymentInstructionBatch").get("input"));
-                    System.out.println(lookup1.get("InstallPaymentInstructionBatch").get("output"));
 
             }
             catch(Exception e){
 
                 e.printStackTrace();
             }
-
+    return ret;
     }
 
-    public static void main(String[] args){
-        parseDOM("src/matcher/compositeAmazonFlexiblePaymentsServiceAPIProfile.wsdl");
+    /*
+    this method calcaltes each combination of operation compparison.
+     */
+    public static void makeDoc( Map<String, Map<String, String>> i ,  Map<String, Map<String, String>>  o){
 
+        for (Map.Entry<String, Map<String, String>> ii : i.entrySet()) {
+            for (Map.Entry<String, Map<String, String>> oi : o.entrySet()) {
+
+               Double optName = EditDistance.getSimilarity(ii.getKey(), oi.getKey());
+               System.out.println("Operation Name: " + ii.getKey() + " & " + oi.getKey() + " = " + optName);
+
+               Double inoutName = EditDistance.getSimilarity(ii.getValue().get("input"), oi.getValue().get("output") );
+                System.out.println("Element Name: "+ ii.getValue().get("input") + " + " + oi.getValue().get("output") + inoutName);
+
+            }
+
+        }
+    }
+    public static void main(String[] args){
+        Map<String, Map<String, String>> input = parseDOM("WSDLs/compositeAmazonFlexiblePaymentsServiceAPIProfile.wsdl");
+        Map<String, Map<String, String>> output = parseDOM("WSDLs/Data8CreditCardValidationAPIProfile.wsdl");
+        makeDoc(input,output);
     }
 
 }
